1e1f9b61f4eb00f25b659548a53a0c0413e4a792
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-11-15 20:04:17 -0700

    Complete ogre

---
+++ exercises/human.js
@@ -0,0 +1,15 @@
+class Human{
+  constructor(name){
+    this.name = name;
+    this.encounterCounter = 0;
+    this.knockedOut = false;
+  }
+  noticesOgre(ogre){
+    if(this.encounterCounter != 0 && this.encounterCounter% 3 === 0){
+      return true;
+    }else
+      return false;
+  }
+}
+
+module.exports = Human
